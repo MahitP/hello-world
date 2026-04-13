bf52cb2a6c5480dbd8658d5107e461953949f7ff
Mahit <124071736+MahitP@users.noreply.github.com>
2026-04-13 17:08:38 -0400

    Initial commit: HelloWorld program
    Add HelloWorld Java program

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld
+{
+	public HelloWorld()
+	{
+		System.out.println("Hello World");
+	}
+
+	public static void main (String[] args)
+	 {
+	 	HelloWorld app = new HelloWorld();
+ 	 }
+
+}
